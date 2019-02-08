846836df0d6d579c38ec9b9c6f0bad8eb8ee37ef
melenadesign <lexymer@gmail.com>
2019-02-08 19:20:30 +0200

    transfer func fix

--- src/services/swapi-service.js
+++ src/services/swapi-service.js
@@ -47,7 +47,7 @@ export default class SwapiService {
         return item.url.match(idRegExp)[1];
     }
 
-    _transformPlanet(planet) {
+    _transformPlanet = (planet) => {
 
         return  {     
             id: this._extractId(planet),
@@ -59,7 +59,7 @@ export default class SwapiService {
     };
     }
 
-    _transformPerson(person) {
+    _transformPerson = (person) => {
 
         return  {     
             id: this._extractId(person),
@@ -71,7 +71,7 @@ export default class SwapiService {
     };
     }
 
-    _transformStarship(starship) {
+    _transformStarship = (starship) => {
 
         return  {     
             id: this._extractId(starship),
